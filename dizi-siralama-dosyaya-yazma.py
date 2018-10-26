64afcf638067224818c7751d6320ba4087002053
fthklyc <44159218+fthklyc@users.noreply.github.com>
2018-10-26 11:44:38 +0300

    Update and rename README.md to dizi-siralama-dosyaya-yazma.py

---
+++ dizi-siralama-dosyaya-yazma.py
@@ -0,0 +1,25 @@
+list = []
+
+
+while True:
+    n = int(input("Kaç adet sayı gireceksiniz:"))
+    if n<=0:
+        print("0'dan büyük değerler giriniz...")
+
+    elif n>0:
+
+        for i in range(n):
+            x = int(input("Bir sayi giriniz:"))
+            list.append(x)
+
+        y = sorted(list,reverse=True) #dizi içini büyükten küçüğe sıralama
+        for j in y:
+            print('{}'.format(j))
+
+        break
+
+with open("C:/Users/Fatih/Desktop/dizisiralama2.txt","a") as dosya:
+    for d in y:
+        #dosya.write("***")
+        #dosya.write("\n")
+        dosya.write('{}\n'.format(str(d)))
